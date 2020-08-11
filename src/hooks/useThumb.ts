b72cdd585e044a164ec4be3bfd25b2e11bc1e082
François BILLIOUD <f.billioud@gmail.com>
2020-08-11 23:52:13 +0200

    Prevent call to onValueChange on mount
    Signed-off-by: François BILLIOUD <f.billioud@gmail.com>

--- src/hooks/useThumb.ts
+++ src/hooks/useThumb.ts
@@ -37,7 +37,7 @@ const useThumb = ({ step, value: propValue, minimumValue, maximumValue, onValueC
       Math.min(hardRounded, maximumValue)
     )
     setValue(withinBounds)
-    onValueChange && onValueChange(withinBounds)
+    onValueChange && value !== withinBounds && onValueChange(withinBounds)
   }
 
   return { updateValue, value }
