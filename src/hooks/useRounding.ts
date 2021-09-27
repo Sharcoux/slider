8549aafc709b03e1f933e2157481930c3464415e
François Billioud <f.billioud@gmail.com>
2021-09-27 14:24:34 +0200

    Fix usage with minValue/maxValue not matching step precision

--- src/hooks/useRounding.ts
+++ src/hooks/useRounding.ts
@@ -13,8 +13,10 @@ type Props = {
 */
 const useRounding = ({ step, minimumValue, maximumValue }: Props) => {
   return React.useCallback((value: number) => {
+    // We tolerate not rounded values when they exactly match the bounds
+    if (value === minimumValue || value === maximumValue) return value
     // Caluculate the precision we need to represent the values
-    const precision = (!step) ? Infinity : Math.max(...[minimumValue, maximumValue, step].map(value => ((value + '').split('.')[1] || '').length))
+    const precision = (!step) ? Infinity : ((step + '').split('.')[1] || '').length
     // Round the value to match the steps
     const rounded = step ? Math.round(value * Math.pow(10, precision) / step) * step / Math.pow(10, precision) : value
     // Ensure that the value is correctly rounded for decimals
